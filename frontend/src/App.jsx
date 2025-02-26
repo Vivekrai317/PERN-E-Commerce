a945a4b78c0520cead77dd3963c4ade528901cd4
Vivekrai317 <vivekrai317@gmail.com>
2025-02-26 16:20:43 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./components/NavBar"
+import HomePage from "./pages/HomePage"
+
+import { Routes,Route } from "react-router-dom"
+import ProductPage from "./pages/ProductPage"
+function App() {
+
+  return (
+   <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme="forest">
+    <NavBar/>
+
+    <Routes>
+      <Route path="/" element = {<HomePage/>}/>
+      <Route path="/product/:id" element={<ProductPage/>}/>
+    </Routes>
+
+   </div>
+  )
+}
+
+export default App
